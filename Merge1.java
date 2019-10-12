55039abadac980e4e1bb7ac71a50ed811cbf6aaf
shrey-1999 <42883586+shrey-1999@users.noreply.github.com>
2019-10-13 01:55:11 +0530

    Add files via upload

---
+++ Merge1.java
@@ -0,0 +1,53 @@
+package MergeSortedArrays;
+
+public class Merge {
+
+	public static int[] merge(int[] arr1,int[] arr2) {
+
+		int[] arr3 = new int[arr1.length + arr2.length];
+		int i
+		int j
+		int k
+
+		while(i<arr1.length && j<arr2.length) {
+
+			if(arr1[i] <=arr2[j]) {
+				arr3[k] = arr1[i];
+				i++;
+				k++;
+			}else {
+				arr3[k] = arr2[j];
+				j++;
+				k++;
+			}
+		}
+
+		while(i<arr1.length) {
+			arr3[k] = arr1[i];
+			k++;
+			i++;
+		}
+
+		while(j<arr2.length) {
+			arr3[k] = arr2[j];
+			k++;
+			j++;
+		}
+
+		return arr3;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		int[] arr1 = { 1,2,5,6,7,9};
+		int [] arr2 = {3,4,6,8,10,12,13};
+
+		int[] mergedArray = merge(arr1,arr2);
+		for(int i=0;i<mergedArray.length;i++) {
+			System.out.println(mergedArray[i]);
+		}
+
+	}
+
+}
