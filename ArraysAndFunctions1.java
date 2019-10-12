3d1ed788e6768ab926d901e592cc8c85ef5417f3
shrey-1999 <shreysawhney1999@gmail.com>
2019-10-13 02:09:50 +0530

    Changes MAde

--- ArraysAndFunctions1.java
+++ ArraysAndFunctions1.java
@@ -26,6 +26,15 @@ public class ArraysAndFunctions {
 		return l;
 		
 	}
+
+	public static void printArray(int[] arr) {
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		
+		
+	}
 	
 	public static void main(String[] args) {
 		
