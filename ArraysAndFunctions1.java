1c68466957e9ee0c5d805751c6173b83c51cd911
shrey-1999 <42883586+shrey-1999@users.noreply.github.com>
2019-10-13 02:01:45 +0530

    Add files via upload

---
+++ ArraysAndFunctions1.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArraysAndFunctions {
+
+	
+	public static int[] takeInput() {
+		
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int[] input = new int[n];
+		for(int i=0;i<n;i++) {
+			input[i] = s.nextInt();
+		}
+		
+		return input;
+	}
+	public static int largest(int[] arr) {
+		int l = Integer.MIN_VALUE;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i] > l) {
+				l = arr[i];
+			}
+		}
+		return l;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		int[] arr = takeInput();
+		printArray(arr);
+		
+		int l = largest(arr);
+		System.out.println(l);
+
+	}
+
+}
